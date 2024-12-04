30efe036283a31180cde31f29abdd50ff5380edf
anuj123-45 <anuj1358.be20@chikara.edu.in>
2024-12-04 12:26:44 +0530

    done with allmodels file

---
+++ server/database/allmodels.js
@@ -0,0 +1,19 @@
+import { FoodModel } from './food'
+import { MenuModel } from './menu'
+import { RestaurantModel } from './restaurant'
+import { UserModel } from './user'
+import { OrderModel } from './order'
+import { ReviewModel } from './review'
+import { ImageModel } from './images'
+
+export {
+    FoodModel,
+    MenuModel,
+    RestaurantModel,
+    UserModel,
+    OrderModel,
+    ReviewModel,
+    ImageModel
+};
+
+
