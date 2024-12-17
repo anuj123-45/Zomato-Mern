bbfd0dee134807ef9678ee1bc8a51c27f1302de7
anuj123-45 <anuj1358.be20@chikara.edu.in>
2024-12-17 11:21:13 +0530

    added validatesingin

--- server/Validation/auth.js
+++ server/Validation/auth.js
@@ -13,3 +13,12 @@ export const validateSignUp = (userData) => {
 
     return Schema.validateAsync(userData);
 }
+
+export const validateSignIn = (userData) => {
+    const Schema = joi.object({
+        email: joi.string().email().required(),
+        password: joi.string().min(5).required(),
+    })
+
+    return Schema.validateAsync(userData);
+}
