8bc4886e0f7e9ae4ffea2e89926a328d88d6c49a
anuj123-45 <anuj1358.be20@chikara.edu.in>
2025-01-01 12:37:51 +0530

    navbar done

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import { PiUserFill } from "react-icons/pi";
+import { FaLocationDot } from "react-icons/fa6";
+import { IoMdArrowDropdown ,IoMdArrowDropup} from "react-icons/io";
+import { RiSearch2Line } from "react-icons/ri";
+
+
 
 const MobileNav = () => {
     return <div className='flex items-center justify-between md:hidden'>
@@ -14,20 +19,34 @@ const MobileNav = () => {
 }
 
 const MediumNav = () => {
-    return <div className='hidden md:block '>
-        <div className='w-32 h-20-'>
+    return <div className='hidden md:flex items-center gap-5 w-full lg:w-3/4'>
+        <div className='w-28'>
             <img src="https://cdn.freelogovectors.net/wp-content/uploads/2024/03/zomato-logo-freelogovectors.net_.png" alt="logo" className='w-full h-full' />
         </div>
-      <div className='w-full bg-white shadow'>
-        <div></div>
-        <div></div>
+      <div className='flex w-full  bg-white shadow-md p-2 border border-gray-200 rounded-md'>
+        <div className='flex items-center gap-2  border-r-2 border-gray-300 pr-2'>
+            <span className='text-zomato-400'><FaLocationDot/></span>
+            <input type="text" placeholder='Bengaluru' className='w-full focus:outline-none'/>
+            <IoMdArrowDropdown/>
+        </div>
+        <div className='flex items-center gap-2 pl-2 w-full'>
+            <RiSearch2Line/>
+        <input type="text" placeholder='Search for restaurant, cuisine or a dish' className='w-full focus:outline-none'/>
+        </div>
+
       </div>
+
+      <div className='flex items-center gap-2 '>
+        <button className='text-gray-400 hover:text-gray-800'>Login</button>
+        <button className='text-gray-400 hover:text-gray-800'>Signup</button>
+      </div>
+
     </div>
 
 }
 const Navbar = () => {
     return <>
-        <nav className='p-3  bg-white shadow-md'>
+        <nav className='py-2 px-10 bg-white shadow-md'>
             <MobileNav />
             <MediumNav />
 
