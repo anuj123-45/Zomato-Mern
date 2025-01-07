888a319b16f951850d656871e4dc857a8354de12
anuj123-45 <anuj1358.be20@chikara.edu.in>
2025-01-07 12:19:17 +0530

    brand carousal done

---
+++ client/src/components/Delivery/Brand.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { NextArrow, PrevArrow } from "../CarousalArrow";
+
+const Brand = () => {
+
+    const logos = [
+        "https://b.zmtcdn.com/data/brand_creatives/logos/1a985408ca7ad8fd097f2c47db9c5cb6_1611252598.png?output-format=webp",
+        "https://b.zmtcdn.com/data/brand_creatives/logos/5bfbd8dfec807f55def6257118c7bf67_1600755551.png?output-format=webp",
+        "https://b.zmtcdn.com/data/brand_creatives/logos/6a11fd0f30c9fd9ceaff2f5b21f61d23_1617187721.png?output-format=webp",
+        "https://b.zmtcdn.com/data/brand_creatives/logos/7a7904eff687a7456599074a4e7d2335_1617875100.png?output-format=webp",
+        "https://b.zmtcdn.com/data/brand_creatives/logos/ec72c289aa01d947fe15a009ce8e559f_1616052816.png?output-format=webp",
+        "https://b.zmtcdn.com/data/brand_creatives/logos/874c2b2b4554f4aed7dd3bb4e755c420_1604383019.png?output-format=webp",
+        "https://b.zmtcdn.com/data/brand_creatives/logos/80c09d718acddee05a655eb378bb442f_1617875125.png?output-format=webp",
+    ];
+    const settings = {
+        arrows: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 6, // Default for large screens
+        slidesToScroll: 1, // Always 1
+        prevArrow: <PrevArrow />,
+        nextArrow: <NextArrow />,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1024, // For medium screens
+                settings: {
+                    slidesToShow: 7,
+                    slidesToScroll: 1,
+                    infinite: true,
+                },
+            },
+            {
+                breakpoint: 600, // For small screens
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    };
+
+    return (
+        <>
+            
+            <div className='my-10 mb-40'>
+            <h1 className="text-xl mb-4 font-semibold">
+                Top Brands in spotlight
+            </h1>
+                <Slider {...settings}>
+                    {
+                        logos.map((logo) => (
+                            <div className='h-32 w-24 p-3'>
+                                <img src={logo} alt="brand" className='h-full w-full' />
+                            </div>
+
+                        ))
+                    }
+                </Slider>
+            </div>
+        </>
+    )
+}
+
+export default Brand
--- client/src/components/Delivery/index.jsx
+++ client/src/components/Delivery/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import DeliveryCarousel from './DeliveryCarousel';
-
+import Brand from './Brand';
 
 function Delivery() {
   return (
     <>
       <DeliveryCarousel />
+      <Brand/>
     </>
   )
 }
