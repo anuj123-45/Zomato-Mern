e3e36e9ae0b63e0d0f3bb291e4d8620f810e1c05
anuj123-45 <anuj1358.be20@chikara.edu.in>
2025-01-09 12:15:31 +0530

    added delivery section

--- client/src/components/Delivery/index.jsx
+++ client/src/components/Delivery/index.jsx
@@ -1,21 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DeliveryCarousel from './DeliveryCarousel';
 import Brand from './Brand';
 import RestaurantCard from '../RestaurantCard';
 
 
 function Delivery() {
+  const [restaurantList, setrestaurantList] = useState([
+    {
+      _id: "12345",
+      photos: ["https://b.zmtcdn.com/data/pictures/chains/8/53088/503a95985aff70b44bc574f0e3e65701.jpg?fit=around|771.75:416.25&crop=771.75:416.25;*,*"],
+      name: "Thalassery Restaurant",
+      cusine: ["Continental", "Oriental", "Asian"],
+      averageCost:400,
+      isPro:false,
+      isOff:100,
+      duarationOfDelivery:39,
+      restaurantReviewValue:4.3,
+    },
+    {
+      _id: "12345-2",
+      photos: ["https://b.zmtcdn.com/data/pictures/0/18369830/e2bf85438d48783f12c79c99ef8ae70c_o2_featured_v2.jpg"],
+      name: "California Burrito",
+      cusine: ["Mexican", "American", "Continental", "Healthy Food", "Beverages", "Fast Food"],
+      averageCost:200,
+      isPro:false,
+      isOff:false,
+      duarationOfDelivery:38,
+      restaurantReviewValue:4.5,
+    },
+    {
+      _id: "12345-3",
+      photos: ["https://b.zmtcdn.com/data/pictures/chains/0/18683480/c9759695c5e14e06927dc556c5f5674f_o2_featured_v2.jpg"],
+      name: "Sri Udupi Food Hub",
+      cusine: ["South Indian", "Chinese", "Biryani", "North Indian", "Sandwich", "Desserts", "Juices", "Beverages"],
+      averageCost:300,
+      isPro:false,
+      isOff:20,
+      duarationOfDelivery:34,
+      restaurantReviewValue:4.3,
+    }
+  ])
+
   return (
     <>
       <DeliveryCarousel />
-      <Brand/>
+      <Brand />
       <div className='flex justify-between flex-wrap'>
-      <RestaurantCard/>
-      <RestaurantCard/>
-      <RestaurantCard/>
-      <RestaurantCard/>
-      <RestaurantCard/>
-      <RestaurantCard/>
+        {
+          restaurantList.map((list) => (
+            <RestaurantCard {...list} key={list._id} />
+          ))
+        }
+
       </div>
     </>
   )
--- client/src/components/RestaurantCard.jsx
+++ client/src/components/RestaurantCard.jsx
@@ -1,30 +1,39 @@
 import React from 'react'
 import { AiTwotoneStar } from 'react-icons/ai'
 
-function RestaurantCard() {
+function RestaurantCard(props) {
   return (
     <>
       <div className='bg-white p-3 w-full rounded-lg md:w-1/2 lg:w-1/3 mb-4 hover:shadow-lg transition duration-700 ease-in-out'>
         <div className='w-full h-40 lg:h-48 relative'>
           <div className='absolute flex justify-between items-end w-full bottom-3'>
             <div className='flex flex-col gap-2 items-start'>
-              <span className='bg-zomato-400 text-white px-2 py-1 rounded text-sm'>Pro extra 10% off</span>
-              <span className='bg-blue-600 text-white px-2 py-1 rounded text-sm'>₹ 80 OFF</span>
+              {
+                props.isPro && (
+                  <span className='bg-zomato-400 text-white px-2 py-1 rounded text-sm'>Pro extra 10% off</span>
+                )
+              }
+              {
+                props.isOff && (
+                  <span className='bg-blue-600 text-white px-2 py-1 rounded text-sm'>₹ {`${props.isOff}`} OFF</span>
+                )
+              }
             </div>
-            <span className='bg-white bg-opacity-75 p-1 rounded mr-3'>42 min</span>
+            <span className='bg-white bg-opacity-75 p-1 rounded mr-3'>{props.duarationOfDelivery} min</span>
           </div>
-          <img src="https://b.zmtcdn.com/data/pictures/0/20547950/df0933fe3bfb6019f25d364fb98e0dd6_o2_featured_v2.jpg?output-format=webp" alt="" className='w-full h-full rounded-lg' />
+          <img src={props.photos.length && props.photos[0]} alt="food" className='w-full h-full rounded-lg' />
         </div>
         <div className='my-2 flex flex-col'>
 
           <div className='flex items-center justify-between '>
-            <h4 className='text-xl font-medium'>Chalukya Samrat Cafe</h4>
-            <span className='bg-green-400 p-1 rounded mr-3  flex items-center text-white'>4.1 <AiTwotoneStar /></span>
+            <h4 className='text-xl font-medium'>{props.name}</h4>
+            <span className='bg-green-400 p-1 rounded mr-3  flex items-center text-white'>{props.restaurantReviewValue} <AiTwotoneStar /></span>
           </div>
 
           <div className='flex items-center justify-between text-gray-500 mr-3'>
-            <p>South Indian, North Indian</p>
-            <p>₹ 350</p>
+          <p>{props.cusine.length > 5 ? props.cusine.slice(0, 3).join(", ") + "..." : props.cusine.join(", ")}</p>
+
+            <p>₹ {props.averageCost} for one</p>
           </div>
 
         </div>
