d5c1abd3a30c1ebfcccdc17639fbc118aee5131f
anuj123-45 <anuj1358.be20@chikara.edu.in>
2025-01-10 11:54:32 +0530

    added dinning carousal

--- client/src/components/Dinning/DinningCarousal.jsx
+++ client/src/components/Dinning/DinningCarousal.jsx
@@ -1,14 +1,31 @@
 import React from 'react'
 import PictureCarousalCard from '../PictureCarousalCard'
-
+import Slider from 'react-slick';
+import { NextArrow, PrevArrow } from "../CarousalArrow";
 
 
 function DinningCarousal() {
+
+    const settings = {
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: <PrevArrow />,
+        nextArrow: <NextArrow />
+    };
+
     return (
         <>
 
-            <div className='w-1/5'>
+            <div className='w-full'>
+                <Slider {...settings}>
                 <PictureCarousalCard />
+                <PictureCarousalCard />
+                <PictureCarousalCard />
+                <PictureCarousalCard />
+                <PictureCarousalCard />
+                </Slider>
             </div>
         </>
     )
--- client/src/components/PictureCarousalCard.jsx
+++ client/src/components/PictureCarousalCard.jsx
@@ -5,16 +5,19 @@ function PictureCarousalCard() {
   return (
     <>
 
-      <div className='w-full h-64 relative'>
+      <div className='w-full h-64 relative px-3'>
         {/* Image */}
-        <img
+      <div className='w-full h-full relative'>
+        
+        {/* Blackish Overlay */}
+        <div className='absolute inset-0 bg-black opacity-25 rounded-lg'></div>
+      <img
           src="https://b.zmtcdn.com/data/pictures/4/20790444/989668ef4432354cdb8f144fa2077faa.jpeg?output-format=webp&fit=around|771.75:416.25&crop=771.75:416.25;*,*"
           alt="dinning"
           className='w-full h-full object-cover transition duration-700 ease-in-out rounded-lg'
         />
+      </div>
 
-        {/* Blackish Overlay */}
-        <div className='absolute inset-0 bg-black opacity-25 rounded-lg'></div>
 
         {/* Content */}
         <div className='absolute bottom-3 text-white left-4'>
