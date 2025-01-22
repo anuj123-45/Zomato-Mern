a6bb363a1afb517b4a5ef72445a738719809698d
anuj123-45 <anuj1358.be20@chikara.edu.in>
2025-01-22 12:42:26 +0530

    bug fixed

--- client/src/components/Restaurant/MenuSimilarRestaurantCard.jsx
+++ client/src/components/Restaurant/MenuSimilarRestaurantCard.jsx
@@ -5,9 +5,8 @@ import { FaStar } from "react-icons/fa";
 const MenuSimilarRestaurantCard = (props) => {
   return (
     <>
- 
- <div className='bg-white shadow-md w-full sm:w-48 md:w-64 lg:w-80 p-2 '>
-  <div className='w-full h-48'>
+ <div className='bg-white shadow-md w-full sm:w-48 md:w-52 lg:w-72 p-2 '>
+  <div className=' lg:w-64 h-48 '>
     <img
       src={props.image}
       alt="food"
@@ -16,29 +15,39 @@ const MenuSimilarRestaurantCard = (props) => {
   </div>
   <div className='flex flex-col gap-2 p-3'>
     <h3 className='font-semibold  my-1 text-lg'>{props.title}</h3>
-    <div className="flex items-center gap-3 text-sm">
-      <span className="flex items-center gap-1">
-        <span className="flex items-center gap-1 bg-green-700 text-white px-2 py-1 rounded">
-          3.3
-          <FaStar />
-        </span>
-        Dining
-      </span>
-      <span className="w-2 h-5 border-r border-gray-500" />
-      <span className="flex items-center gap-1">
-        <span className="flex items-center gap-1 bg-green-700 text-white px-2 py-1 rounded">
-          4.2
-          <FaStar />
-        </span>
-        Delivery
-      </span>
-    </div>
+
+
+    <div className="flex flex-wrap items-center gap-3 text-sm">
+  {/* Dining Section */}
+  <span className="flex items-center gap-1">
+    <span className="flex items-center gap-1 bg-green-700 text-white px-2 py-1 rounded">
+      3.3
+      <FaStar />
+    </span>
+    Dining
+  </span>
+
+  {/* Divider */}
+  <span className="hidden md:block w-2 h-5 border-r border-gray-500" />
+
+  {/* Delivery Section */}
+  <span className="flex items-center gap-1">
+    <span className="flex items-center gap-1 bg-green-700 text-white px-2 py-1 rounded">
+      4.2
+      <FaStar />
+    </span>
+    Delivery
+  </span>
+</div>
+
+    
     <div className='flex flex-col'>
       <h4>North Indian, Mughlai, Chinese</h4>
       <p className='text-gray-400'>Marathahalli, Bangalore</p>
     </div>
   </div>
 </div>
+
 
 
     </>
